7598752d401a04eb4d4760b2e0805e68abc81f8d
N.Shimizu <chikoski@gmail.com>
2015-04-23 10:37:51 +0900

    fixed a bug in test case 03

--- tests/03.js
+++ tests/03.js
@@ -1,7 +1,7 @@
 describe("getHour: 経過後の時間を求める関数", function(){
   it("第1引数に指定した時間から、第2引数に指定した時間が経過したのちの時間を計算する", function(){
     getHour(0, 0).should.equal(0);
-    getHour(1, 0).should.equal(0);
+    getHour(1, 0).should.equal(1);
     getHour(23, 1).should.equal(0);
     getHour(2, 4).should.equal(6);
     getHour(26, 4).should.equal(null);
